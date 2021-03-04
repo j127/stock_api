6bacdf2ac010d19459c5d2c6ece27f627db47acb
J. Cohen <hostelmarketing@gmail.com>
2021-03-03 23:42:31 -0800

    Add cors

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 const PORT = 5000;
 const API_URL = process.env.API_URL;
