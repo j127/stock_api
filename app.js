b210c937ab31114cd93dfc522cb7f38243337049
J. Cohen <hostelmarketing@gmail.com>
2021-03-04 00:21:30 -0800

    Update for APCA

--- app.js
+++ app.js
@@ -10,10 +10,17 @@ app.use(cors());
 
 const PORT = 5000;
 const API_URL = process.env.API_URL;
+const APCA_API_KEY_ID = process.env.APCA_API_KEY_ID;
+const APCA_API_SECRET_KEY = process.env.APCA_API_SECRET_KEY;
 
 app.get("/", async (req, res) => {
     try {
-        const response = await axios.get(API_URL);
+        const response = await axios.get(API_URL, {
+            headers: {
+                "APCA-API-KEY-ID": APCA_API_KEY_ID,
+                "APCA-API-SECRET-KEY": APCA_API_SECRET_KEY,
+            },
+        });
         const data = response.data;
         res.json(data);
     } catch (e) {
